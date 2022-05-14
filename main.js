4beefbd541b65680dd393c533e288a352a339de3
Sergey <sergeydzubenko1@gmail.com>
2022-05-14 11:58:53 +0300

    commit

--- main.js
+++ main.js
@@ -57,33 +57,3 @@ b.addEventListener('click', function () {
 
 
 
-class TodoList {
-    constructor(items) {
-        this.items = [];
-    }
-
-    add(params) {
-        const newTodoItem = new TodoItem(params);
-        this.items.push(newTodoItem);
-    }
-
-        completeAll(id) {
-            const toDo = this.items.find((item) => item.id === id);
-            toDo.done = true;
-    }
-}
-class TodoItem {
-    constructor(id, title, description, done) {
-        this.id = id;
-        this.title = Text;
-        this.description = prompt("Краткое описание");
-        this.done = false;
-    }
-}
-const newList = new TodoList();
-
-newList.add(1);
-newList.add(2);
-newList.add(3);
-newList.completeAll(2);
-console.log(newList.items);
